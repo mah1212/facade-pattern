76a7cf68dde2dae59fd5c717cbe6cbcaa7fb2dab
mmrob <mrob005@gmail.com>
2014-08-08 11:42:45 +0600

    Facade Pattern

---
+++ FacadePattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ShapeBuilder shapeBuilder = new ShapeBuilder();
+
+            shapeBuilder.DrawCircle();
+            shapeBuilder.DrawRectangle();
+            shapeBuilder.DrawSquare();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FacadePattern/ShapeBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    public class ShapeBuilder
+    {
+        private IShape circle;
+        private IShape rectangle;
+        private IShape square;
+
+        public ShapeBuilder()
+        {
+            circle = new Circle();
+            rectangle = new Rectangle();
+            square = new Square();
+        }
+
+        public void DrawCircle()
+        {
+            circle.Draw();
+        }
+
+        public void DrawRectangle()
+        {
+            rectangle.Draw();
+        }
+
+        public void DrawSquare()
+        {
+            square.Draw();
+        }
+    }
+    
+}
